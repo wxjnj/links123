b37647a2e7a97da058f6cf82f254733bd1d7697e
Jim <neverok@gmail.com>
2013-08-08 11:49:22 +0800

    replace mysql

--- links123/App/Conf/config.php
+++ links123/App/Conf/config.php
@@ -2,7 +2,7 @@
 return array(
     'URL_MODEL'                 =>  2,										// 如果你的环境不支持PATHINFO 请设置为3
     
-    'DB_TYPE'                   =>  'mysql',
+    'DB_TYPE'                   =>  'mysqli',
 	'DB_HOST'                   =>  '127.0.0.1',
 	'DB_NAME'                   =>  'linkstest',
 	'DB_USER'                   =>  'linkstest',
