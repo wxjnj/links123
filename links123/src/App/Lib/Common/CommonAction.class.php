5c7d9c26ec20b3dc04d5bccc7f6eb61feeb53325
frank <312181918@qq.com>
2013-08-15 15:42:02 +0800

    自动登录代码的重构-autologin

--- links123/src/App/Lib/Common/CommonAction.class.php
+++ links123/src/App/Lib/Common/CommonAction.class.php
@@ -14,7 +14,7 @@ class CommonAction extends Action {
 		//网站升级
 		$this->updating();
 		//自动登录
-		//$this->autoLogin();
+		$this->autoLogin();
 
 		$variable = $this->_getVariable();
 		$this->assign('cn_tip', $variable['cnTip']);
@@ -233,36 +233,29 @@ class CommonAction extends Action {
 
     /**
      * 自动登录
+     * @author frank qian 2013-08-15
      */
     public function autoLogin() {
-        $user_str = cookie("USER_ID");
-        if (!isset($_SESSION[C('MEMBER_AUTH_KEY')]) || empty($_SESSION[C('MEMBER_AUTH_KEY')])) {
-            if (!empty($user_str)) {
-                $ret = explode("|", $user_str);
-                $user_id = intval($ret[0]);
-                $user_info = D("Member")->find($user_id);
-                if (!empty($user_info) && md5($user_info['password'] . $user_info['nickname']) == $ret[1]) {
-                    $_SESSION[C('MEMBER_AUTH_KEY')] = $user_info['id'];
-                    $_SESSION['nickname'] = $user_info['nickname'];
-                    $_SESSION['face'] = $user_info['face'];
-                    if (empty($_SESSION['face'])) {
-                        $_SESSION['face'] = "face.jpg";
-                    }
-                    //使用cookie过期时间来控制前台登陆的过期时间
-                    $home_session_expire = D("Variable")->getVariable("home_session_expire");
-                    cookie(md5("home_session_expire"), time(), $home_session_expire);
-                }
+        $user_str = $_COOKIE["USER_ID"];
+        //没有用户session且自动登录标示Cookie USER_ID 存在执行自动登录过程
+        if ((!isset($_SESSION[C('MEMBER_AUTH_KEY')]) || empty($_SESSION[C('MEMBER_AUTH_KEY')])) && !empty($user_str)) {
+            $ret = explode("|", $user_str);
+            $user_id = intval($ret[0]);
+            $user_info = D("Member")->find($user_id);
+            
+            if (!empty($user_info) && md5($user_info['password'] . $user_info['nickname']) == $ret[1]) {
+            	$_SESSION[C('MEMBER_AUTH_KEY')] = $user_info['id'];
+                $_SESSION['nickname'] = $user_info['nickname'];
+                $_SESSION['face'] = empty($user_info['face'])?'face.jpg':$user_info['face'];
+                    
+                //使用cookie过期时间来控制前台登陆的过期时间
+                $home_session_expire = intval(D("Variable")->getVariable("home_session_expire"));
+                cookie(md5("home_session_expire"), time(), $home_session_expire);
             }
-        } else {
-            if (empty($user_str)) {
-                //根据cookie检查session是否过期
-                $time = cookie(md5("home_session_expire"));
-                if (empty($time)) {
-                    unset($_SESSION[C('MEMBER_AUTH_KEY')]);
-                    unset($_SESSION['nickname']);
-                    unset($_SESSION['face']);
-                }
-            }
+        } else if(empty($user_str) && empty($_COOKIE[md5("home_session_expire")])) {//自动登录标示Cookie USER_ID 时间过期
+        	unset($_SESSION[C('MEMBER_AUTH_KEY')]);
+            unset($_SESSION['nickname']);
+            unset($_SESSION['face']);    
         }
     }
 
