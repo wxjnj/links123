797d6e2b4eb81b1c015543fb7ffedcf7bac00495
frank <312181918@qq.com>
2013-08-17 18:43:57 +0800

    整理了目录结构

--- links123/src/App/Lib/Action/Home/EmptyAction.class.php
+++ links123/src/App/Lib/Action/Home/EmptyAction.class.php
@@ -1,9 +1,19 @@
 <?php 
+/**
+ * @desc 404错误页面
+ * @name EmptyAction.class.php
+ * @package　Home
+ * @author Frank UPDATE 2013-08-17
+ * @version 0.0.1
+ */
 import("@.Common.CommonAction");
-class EmptyAction extends CommonAction{ 
-	//404页面
-	public function _empty(){ 
-		header("HTTP/1.0 404 Not Found");//使HTTP返回404状态码 
+class EmptyAction extends CommonAction { 
+	/*
+	 * @desc 404错误页面
+	 * @author Frank UPDATE 2013-08-17
+	 */
+	public function _empty() { 
+		header("HTTP/1.0 404 Not Found");
 		$this->title = "真的很抱歉，我们搞丢了页面……";
 		$this->display(C('404_PAGE'));
 	}
