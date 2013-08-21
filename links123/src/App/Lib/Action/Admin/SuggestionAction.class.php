1f1e41c17aff1b65184d8e94158336c7766ed209
lee <285920192@qq.com>
2013-08-21 16:13:22 +0800

    后台留言板重构

--- links123/src/App/Lib/Action/Admin/SuggestionAction.class.php
+++ links123/src/App/Lib/Action/Admin/SuggestionAction.class.php
@@ -1,76 +1,96 @@
 <?php
-
 /**
- * @name UserAction.class.php
+ * @name SuggestionAction.class.php
  * @package Admin
- * @desc 建议投诉
- * @author lawrence UPDATE 2013-08-20
+ * @desc 后台管理-留言板
+ * @author Lee UPDATE 2013-08-20
  * @version 0.0.1
  */
-class SuggestionAction extends CommonAction {
 
+class SuggestionAction extends CommonAction {
+    
+	/**
+	 * @desc 留言板查询条件
+     * @author Lee UPDATE 2013-08-20
+	 * @param array $map SQL条件数组
+     * @param array $param 参数数组
+	 * @return array    
+	 */    
 	protected function _filter(&$map, &$param){
-		if (isset($_REQUEST['suggest']) && !empty($_REQUEST['suggest']) ) {
-			$map['suggest'] = array('like',"%".$_REQUEST['suggest']."%");
+		if (isset($_REQUEST['suggest']) && !empty($_REQUEST['suggest'])) {
+			$map['suggest'] = array('like', "%".$_REQUEST['suggest']."%");
 			$this->assign("suggest", $_REQUEST['suggest']);
 			$param['suggest'] = $_REQUEST['suggest'];
 		}
+        
 		if (isset($_REQUEST['type']) && $_REQUEST['type']!='') {
 			$map['type'] = $_REQUEST['type'];
 			$this->assign('type', $_REQUEST['type']);
 			$param['type'] = $_REQUEST['type'];
 		}
+        
+        $map['status'] = array('egt', 0);
 		if (isset($_REQUEST['status']) && $_REQUEST['status']!='') {
 			$map['status'] = $_REQUEST['status'];
 			$this->assign('status', $map['status']);
 			$param['status'] = $map['status'];
 		}
-		else {
-			$map['status'] = array('egt', 0);
-		}
 	}
-	
+	     
 	/**
-	 * @desc 默认首页
-	 * @see SuggestionAction::index()
-	 */
-	public function index() {
+	 * @desc 留言板首页
+     * @author Lee UPDATE 2013-08-20  
+	 */      
+	public function index(){
 		$map = array();
 		$param = array();
-		if (method_exists($this,'_filter')) {
-			$this->_filter($map,$param);
+		if (method_exists($this, '_filter')) {
+			$this->_filter($map, $param);
 		}
+        
 		$model = new SuggestionViewModel();
 		if (!empty($model)) {
-			$this->_list($model,$map, $param,'id',false);
+			$this->_list($model, $map, $param, 'id', 'DESC');
 		}
+        
 		$this->display();
-		return;
 	}
 	
 	/**
-	 * @desc 编辑页面
-	 * @see SuggestionAction::edit()
+	 * @desc 留言板回复页面
+     * @author Lee UPDATE 2013-08-20  
 	 */
 	function edit() {
-		$model = M("Suggestion" );
+		$model = M("Suggestion");
+              
+        if (empty($_REQUEST["id"])) {
+           $this->error('参数不能为空!'); 
+        }
+
 		$vo = $model->getById($_REQUEST["id"]);
-		$vo["create_time"] = date('Y-m-d H:i:s',$vo["create_time"]);
-		$this->assign('vo',$vo );
+        if (empty($vo)) {
+            $this->error('参数错误!'); 
+        }
+        
+		$vo["create_time"] = date('Y-m-d H:i:s', $vo["create_time"]);
+		$this->assign('vo', $vo);
+        
 		$this->display();
 	}
 	
 	/**
-	 * @desc 编辑操作
-	 * @see SuggestionAction::update()
+	 * @desc 留言板回复保存
+     * @author Lee UPDATE 2013-08-20 
 	 */
 	function update() {
-		$model = D( "Suggestion" );
+		$model = D("Suggestion");
+        
 		if (false === $model->create()) {
 			$this->error($model->getError());
 		}
+        
         $model->__set('is_reply', 1);
-        $model->__set('create_time',time());
+        $model->__set('create_time', time());
         $model->__set('mid', -1);
 		$list = $model->add();
 		if (false !== $list) {
