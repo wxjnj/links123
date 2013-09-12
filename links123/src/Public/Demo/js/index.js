b8b8019617871b6ba55fdb97010fffd7cc7efa79
slate <itphper@qq.com>
2013-09-12 22:51:00 +0800

    * 首页3.0 Demo 数据填充提交

--- links123/src/Public/Demo/js/index.js
+++ links123/src/Public/Demo/js/index.js
@@ -10,8 +10,54 @@ var PUBLIC = $('#J_PUBLIC').val();
 
 $(function() {
 
+	THL.init();
+	
 	$('.skins').on('click', function(){
 		$('#J_Skins').attr('href', '__PUBLIC__/Demo/skins/light/style.css');
+	});
+	
+	// 弹出页
+	$(".newWin").live('click', function() {
+		window.open($(this).attr('url'));
+	});
+	
+	// 直达框
+	$(document).on('click', function(){
+		$('#direct_text').val($('#direct_text').attr('txt')).removeClass('ipson');
+	});
+	$(".J_header_top").on('mouseenter', function(){
+		var tag = $.trim($('#direct_text').val());
+		if(tag == $('#direct_text').attr('txt')){
+			$("#direct_text").select().addClass('ipson');
+		}else{
+			$("#direct_text").addClass('ipson');
+		}
+	}).on('mouseleave', function(){
+		var tag = $.trim($('#direct_text').val());
+		if(tag == '' || tag == $('#direct_text').attr('txt')){
+			$('#search_text').select();
+			$('#direct_text').removeClass('ipson');
+		}
+	});
+	$("#direct_text").on('click', function(){
+		var tag = $.trim($('#direct_text').val());
+		if (tag == $('#direct_text').attr('txt')){
+			$('#direct_text').val('').addClass('ipson');
+		}
+		return false;
+	});
+
+	$('.J_direct_submit').on('click', function(){
+		$("#frm_drct").trigger('submit');
+		$("#direct_text")[0].focus();
+		return false;
+	});
+	$("#frm_drct").on('submit', function(){
+		var tag = $.trim($('#direct_text').val());
+		if (tag == '' || tag == $('#direct_text').attr('txt')){
+			return false;
+		}
+		$('#direct_text').select();
 	});
 	
 	//保存自留地网址
@@ -56,6 +102,7 @@ $(function() {
 	
 	//登录窗口
 	$('.J_signin').click(function(){
+		$('#J_signup_form').dialog('close');
 		 $('#J_signin_form').dialog('open');
 		 $('.lk-dialog-login').show();
 		 $('.ui-dialog-titlebar').hide();
@@ -64,6 +111,7 @@ $(function() {
 	
 	//注册窗口
 	$('.J_signup').click(function(){
+		$('#J_signin_form').dialog('close');
 		$("#verifyImg").trigger("click");
 		$('#J_signup_form').dialog('open');
 		$('.lk-dialog-reg').show();
@@ -71,9 +119,10 @@ $(function() {
 		return false;
 	});
 	
-	$('#J_forgetpass').click(function(){
+	$('.J_forgetpass').click(function(){
+		$('#J_signin_form').dialog('close');
 		$('#J_forgetpass_form').dialog('open');
-		$('.reglogin').show();
+		$('.lk-dialog-fpass').show();
 		$('.ui-dialog-titlebar').hide();
 		return false;
 	});
@@ -123,7 +172,7 @@ $(function() {
 		$('#J_forgetpass_form').dialog('close');
 	});
 	
-	$('#verifyImg').click(function(){
+	$('.J_verifyImg').click(function(){
 		var timenow = new Date().getTime();
 		$(this).attr("src", APP+'Verify?'+timenow);
 
@@ -259,4 +308,142 @@ $(function() {
 	});
 	
 });
+
+/* 糖葫芦 */
+var THL = {
+	conf : {
+		topnm : 40,
+		topex : 65
+	},
+	init : function(){
+		var self = this;
+
+		//重新加载页面清除keyword 清除浏览器未清除的文本框的值
+		$.cookies.set('keyword', '');
+		$("#search_text").val('').select(); //选中文本框
+		
+		$('.thl').mouseover(function(){ $('#J_thl_div').show(); }); //移入糖葫芦区域 显示糖葫芦
+		$('.J_thl_area').mouseleave(function(){ $('#J_thl_div').hide(); }); //移除糖葫芦主区域 隐藏糖葫芦
+		
+		$(".J_thlz a").click(function(){ //糖葫芦籽点击
+			$(this).addClass("on").siblings("a").removeClass("on");
+			$("#btn_search").trigger("click");
+			return false;
+		});
+		
+		$("#J_thl_div a").click(function(){ //糖葫芦点击
+			$(this).addClass("on").siblings("a").removeClass("on");
+			var index = $(this).index();
+			$(".J_thlz:eq(" + index + ")").show().siblings(".J_thlz").hide();
+			$(".J_thlz a").removeClass("on");
+			$(".J_thlz:eq(" + index + ")").find("a:first").addClass("on");
+			return false;
+		});
+
+		$("#search_text").blur(function(){ //文本框失去焦点 如 内容为空 清除keyword
+			if ($(this).val() == '') {
+				$.cookies.set('keyword', '');
+			}
+		});
+		
+		$("#search_text").keyup(function(event){ //文本框输入内容 设置糖葫芦 位置
+			$('#J_thl_div').show();
+			self.setpos();
+		});
+
+		$(".thl").mouseenter(function(){ $("#search_text").select(); }); //移入 糖葫芦 选中 文本
+
+		$('#search_text').on('click', function(){
+			if (!$.cookies.get('keyword')) {
+				var key  = $.trim($("#search_text").val());
+				if(key != ''){
+					$(this).data('key', key);
+					$(this).val(key);
+				}
+			} else {
+				
+				$(this).data('key', '');
+			}
+		});
+
+		$('#search_text').on('webkitspeechchange', function(){ //onwebkitspeechchange
+			var key = $(this).data('key');
+			if(key && key != ''){
+				var v = $(this).val();
+				if(v.indexOf(key) == 0){
+					$(this).val(v.replace(key, key+' '));
+					$(this).data('key', '');
+				}
+			}
+			self.setpos();
+		}); 
+
+		$(document).mouseup(function(ev){ // 搜索文本框始终获取焦点
+			if ( document.activeElement.tagName == "INPUT" 
+				|| document.activeElement.tagName == "TEXTAREA" 
+				|| document.activeElement.tagName == "IFRAME"
+				|| document.activeElement.id == "direct_text"
+				|| document.activeElement.id == "search_text"
+			) {
+				return;
+			}
+			var txt = '';
+			if (window.getSelection){ // mozilla FF 
+				txt = window.getSelection();
+			}else if(document.getSelection){
+				txt = document.getSelection();
+			}else if(document.selection){ //IE
+				txt = document.selection.createRange().text;
+			}
+			if (txt == '') { $("#search_text").select(); } //未划选文本 划选 文本框
+			if ($.cookies.get('keyword')){ $("#search_text").select(); } //有keyword时 直接划选 文本框
+		});
+
+		$("#btn_search").click(function() { //单击搜索按钮
+			$("#search_text").select();
+			var keyword  = $.trim($("#search_text").val());
+			$.cookies.set('keyword', keyword); //保存keyword
+			keyword = keyword.replace('http://','');
+			keyword = encodeURIComponent(keyword);
+			var url = $(".J_thlz a.on").attr("url").replace('keyword', keyword);
+			var tid = $(".J_thlz a.on").attr("tid");
+			self.go(url, tid, keyword);
+			return false;
+		});
+	},
+	go : function(url, tid, keyword){
+		if (tid == '4' || tid == '40' || tid == '58' || tid == '110' || tid == '117') { // 谷歌、美试、啪啪、PQuora
+			$.post(URL + "/thl_count", {tid : tid}, function() {});
+			window.open("http://" + url);
+		} else if (tid == '10' || tid == '20') { // 另客、维修
+			window.open(url);
+		} else {	
+			url = APP + "Thl/index";
+			//window.open(url);
+			//因window.open会被浏览器阻止，所以才用表单提交
+			var searchFormObj = $('#searchForm');
+			
+			$('#J_thl').val($("#J_thl_div a.on").text());
+			$('#J_tid').val(tid);
+			$('#J_q').val(keyword);
+			
+			searchFormObj.attr('action', url);
+			searchFormObj.attr('target', '_blank');
+			searchFormObj.submit();
+			searchFormObj.attr('action', '');
+			searchFormObj.attr('target', '');
+			$("#search_text").select();
+		}
+	},
+	setpos : function(){
+		var top, self = this;
+		if($("#search_text").val() != ''){ //文本框有值调整位置
+			top = self.conf.topex;
+		}else{
+			top = self.conf.topnm;
+			$('#J_thl_div').hide(); //没值隐藏
+		}
+		$("#J_thl_div").css("top", top);		
+	}
+}
 
