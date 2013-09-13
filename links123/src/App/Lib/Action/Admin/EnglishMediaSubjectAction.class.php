d53046b176ac66c4b83bacdf55cfcc64d96826be
Adam <foureyed@qq.com>
2013-09-13 13:52:22 +0800

    英语角后台，科目、等级、专题、推荐排序功能

--- links123/src/App/Lib/Action/Admin/EnglishMediaSubjectAction.class.php
+++ links123/src/App/Lib/Action/Admin/EnglishMediaSubjectAction.class.php
@@ -32,6 +32,30 @@ class EnglishMediaSubjectAction extends CommonAction {
         $this->assign('vo', $vo);
         $this->display();
     }
+    // 排序
+    public function sort(){
+    	$model = D("EnglishMediaSubject");
+    	$map = array();
+    	if (!empty($_GET['sortId'])) {
+    		$map['id'] = array('in', $_GET['sortId']);
+    	}
+    	else {
+    		$params = explode("&", $_SESSION[C('SEARCH_PARAMS_KEY')]);
+    		foreach ($params as &$value) {
+    			$temp = explode("=", $value);
+    			if ( !empty($temp[1]) && $temp[0] != 'sort' && $temp[0] != 'order' ) {
+    				$map[$temp[0]] = $temp[1];
+    			}
+    		}
+    	}
+    	$sortList = $model->where($map)->order('sort asc')->select();
+    	foreach ($sortList as $key=>$value) {
+    		$sortList[$key]['txt_show'] = $value['name'];
+    	}
+    	$this->assign("sortList", $sortList);
+    	$this->display("../Public/sort");
+    	return;
+    }
 
 }
 
--- links123/src/App/Lib/Action/Admin/EnglishObjectAction.class.php
+++ links123/src/App/Lib/Action/Admin/EnglishObjectAction.class.php
@@ -41,6 +41,30 @@ class EnglishObjectAction extends CommonAction {
             $this->ajaxReturn("", "操作成功", true);
         }
     }
+    // 排序
+    public function sort(){
+    	$model = D("EnglishObject");
+    	$map = array();
+    	if (!empty($_GET['sortId'])) {
+    		$map['id'] = array('in', $_GET['sortId']);
+    	}
+    	else {
+    		$params = explode("&", $_SESSION[C('SEARCH_PARAMS_KEY')]);
+    		foreach ($params as &$value) {
+    			$temp = explode("=", $value);
+    			if ( !empty($temp[1]) && $temp[0] != 'sort' && $temp[0] != 'order' ) {
+    				$map[$temp[0]] = $temp[1];
+    			}
+    		}
+    	}
+    	$sortList = $model->where($map)->order('sort asc')->select();
+    	foreach ($sortList as $key=>$value) {
+    		$sortList[$key]['txt_show'] = $value['name'];
+    	}
+    	$this->assign("sortList", $sortList);
+    	$this->display("../Public/sort");
+    	return;
+    }
 
 }
 
