0d4b9d561d5749c829f5a2fefd572896e4fd302d
jack <19258390@qq.com>
2013-09-14 20:30:17 +0800

    直达栏修改，自留地代码移动，fix密码框背景

--- links123/src/Public/Demo/js/app.js
+++ links123/src/Public/Demo/js/app.js
@@ -1,6 +1,5 @@
 $(function(){
 	User.Init();
-	Zld.Init();
 	THL.Init();
 });
 var User = {
@@ -74,7 +73,7 @@ var User = {
 				}
 			});
 
-			obj.find('input[type="text"], input[type="password"').on('focus', function(){
+			obj.find('input[type="text"], input[type="password"]').on('focus', function(){
 				$(this).css('background', '#fff');
 			}).on('blur', function(){
 				$(this).css('background', '#eeefef');
@@ -200,7 +199,7 @@ var User = {
 				}
 			});
 
-			obj.find('input[type="text"], input[type="password"').on('focus', function(){
+			obj.find('input[type="text"], input[type="password"]').on('focus', function(){
 				$(this).css('background', '#fff');
 			}).on('blur', function(){
 				$(this).css('background', '#eeefef');
@@ -302,7 +301,7 @@ var User = {
 				}
 			});
 
-			obj.find('input[type="text"], input[type="password"').on('focus', function(){
+			obj.find('input[type="text"], input[type="password"]').on('focus', function(){
 				$(this).css('background', '#fff');
 			}).on('blur', function(){
 				$(this).css('background', '#eeefef');
@@ -350,238 +349,7 @@ var User = {
 		}
 	}
 };
-var Zld = {
-	IsSortable: false, //是否为拖拽点击，true则不打开自留地网址
-	Init: function(){
-		var self = this;
-		var obj = $('#J_ZldList');
 
-		$(document).on('click', '#J_ZldList .add', function(){
-			if(self.CheckLogin()){
-				self.Create();
-			}
-		});
-		$(document).on('click',  '#J_ZldList .ctl', function(){
-			if(self.CheckLogin()){
-				if($(this).hasClass('add')){ return false; }
-				var o = $(this).closest('li');
-				var id = o.data('id');
-				var nm = o.find('b').html();
-				var url = o.data('url');
-				self.Create(id, nm, url);
-				return false;
-			}
-		});
-		$(document).on('click', '#J_ZldList .nm', function(){
-			if (!Zld.IsSortable) {
-				var o = $(this).closest('li');
-				var url = o.data('url');
-				self.Go(url);
-			} else {
-				Zld.IsSortable = false;
-			}
-			return false;
-		});
-
-		$('#J_sortable').sortable({
-			update: function(event, ui){
-				Zld.IsSortable = true;
-				
-				$.post(
-					URL + '/sortArealist', 
-					{'area' : $(this).sortable('toArray')},
-					function(data) {
-						if (data == 1) {
-							//成功
-						} else if (data == 0){
-							//失败
-						} else {
-							//失败
-						}
-					}
-				);
-			}  
-		});
-		$('#J_sortable').sortable('enable');
-		
-		$(document).on('click', '#J_Zld .lkd-add, #J_Zld .lkd-edit', function(){
-
-			var o = $('#J_Zld');
-
-			var id = o.find('input[name="id"]').val();
-			var name = o.find('input[name="name"]').val();
-			var url = o.find('input[name="url"]').val();
-
-			if (!name) {
-				alert("请输入网站名称");
-				return false;
-			}
-			if (!url) {
-				alert("请输入网址");
-				return false;
-			}
-			
-			$.post(
-				URL + '/updateArea', 
-				{ 'web_id': id, 'web_url': url, 'web_name': name },
-				function(data) {
-					var licur = function(){
-						var li = null;
-						obj.find('ul>li').each(function(){
-							if($(this).data('id') == id){
-								li = $(this);
-								return;
-							}
-						});
-						return li;
-					}
-					if(data == 1){ //更新成功
-						var li = licur();
-						li.attr('url', '/Link/index.html?mod=myarea&amp;url=' + url);
-						li.data('url', url);
-						li.find('b').html(name);
-					}else if(data > 1){ //新加成功
-						var li = obj.find('.add').closest('li');
-						li.before(self.CreateItem(data, name, url));
-					}else if(data == -1){
-						User.Login('请先登录');
-					}else{
-						alert('操作失败');
-					}
-					o.dialog('close');
-				}
-			);
-			return false;
-		});
-		
-		$(document).on('click', '#J_Zld .lkd-del', function(){
-
-			var o = $('#J_Zld');
-			var id = o.find('input[name="id"]').val();
-			
-			$.post(
-				URL + '/delArea', 
-				{ 'web_id': id },
-				function(data) {
-					var licur = function(){
-						var li = null;
-						obj.find('ul>li').each(function(){
-							if($(this).data('id') == id){
-								li = $(this);
-								return;
-							}
-						});
-						return li;
-					}
-					if(data == 1){
-						var li = licur();
-						li.remove();
-					}else if(data == -1){
-						User.Login('请先登录');
-					}else{
-						alert('操作失败');
-					}
-					o.dialog('close');
-				}
-			);
-			return false;
-		});
-	},
-	CheckLogin: function(){
-		if($CONFIG.IsLogin){
-			User.Login('请先登录');
-			return false;
-		}
-		return true;
-	},
-	Go: function(url){
-		var obj = $('#J_MyAreaForm');
-		obj.find('input[name="url"]').val(url);
-		obj.submit();
-	},
-	Create: function(id, nm, url){
-		if(!$('#J_Zld').size()){
-			var hl = '';
-			hl = hl + '<div class="lk-dialog lk-dialog-zld" id="J_Zld">';
-			hl = hl + '	<div class="lkd-hd">';
-			hl = hl + '		<a class="close" href="javascript:;">X</a>';
-			hl = hl + '	</div>';
-			hl = hl + '	<div class="lkd-bd">';
-			hl = hl + '		<form action="">';
-			hl = hl + '			<ul>';
-			hl = hl + '				<li><input class="ipt" type="text" name="name" placeholder="网站名称" /></li>';
-			hl = hl + '				<li><input class="ipt" type="text" name="url" placeholder="网址" /></li>';
-			hl = hl + '			</ul>';
-			hl = hl + '		</form>';
-			hl = hl + '	</div>';
-			hl = hl + '	<div class="lkd-ft">';
-			hl = hl + '		<input type="hidden" name="id" value="" />';
-			hl = hl + '		<span class="editp" style="display:none;"><a class="lkd-edit" href="javascript:;">确认编辑</a>';
-			hl = hl + '		<a class="lkd-del" href="javascript:;">删除</a></span>';
-			hl = hl + '		<span class="addp"><a class="lkd-add" href="javascript:;">确认添加</a></span>';
-			hl = hl + '	</div>';
-			hl = hl + '</div>';
-			$('body').append(hl);
-
-			var obj = $('#J_Zld');
-
-			obj.dialog({
-				autoOpen: false,
-				width: 384,
-				modal: true,
-				resizable: false,
-				open: function(){
-					setTimeout(function(){obj.find('input[name="name"]').select();}, 20);
-				}
-			});
-
-			obj.find('.close').on('click', function(){
-				obj.dialog('close');
-				return false;
-			});
-
-			obj.find('input[type="text"]').on('focus', function(){
-				$(this).css('background', '#fff');
-			}).on('blur', function(){
-				$(this).css('background', '#eeefef');
-			});
-
-			obj.find('input[name="name"],input[name="url"]').on('keydown', function(event){
-				if (event.keyCode == 13) {
-					if(obj.find('.editp').is(":visible")){
-						obj.find('.lkd-edit').trigger('click');	
-					}else{
-						obj.find('.lkd-add').trigger('click');
-					}
-				}
-			});
-		}
-
-		var obj = $('#J_Zld');		
-		if(id){
-			obj.find('input[name="id"]').val(id);
-			obj.find('input[name="name"]').val(nm);
-			obj.find('input[name="url"]').val(url);
-			obj.find('.editp').show();
-			obj.find('.addp').hide();
-		}else{
-			obj.find('input[name="id"]').val('');
-			obj.find('input[name="name"]').val('');
-			obj.find('input[name="url"]').val('');
-			obj.find('.editp').hide();
-			obj.find('.addp').show();
-		}
-
-		obj.dialog('open');
-	},
-	CreateItem: function(id, nm, url){
-		var hl = '<li id="'+ id +'" url="/Link/index.html?mod=myarea&amp;url='+ url +'" data-id="'+ id +'" data-url="'+ url +'">';
-		hl = hl + '<span class="nm"><i class="mask"></i><b>'+ nm +'</b></span>';
-		hl = hl + '<span class="ctl"><i class="mask"></i></span>';
-		hl = hl + '</li>';
-		return hl;
-	}
-};
 var THL = {
 	conf : {
 		topnm : 36,
--- links123/src/Public/Demo/js/index.js
+++ links123/src/Public/Demo/js/index.js
@@ -4,106 +4,299 @@ var PUBLIC = $CONFIG['PUBLIC'];
 
 $(function() {
 
+	Zld.Init();
+	ZhiDaLan.Init();
+
+	// 用户菜单
 	$('.uc-menu').hover(
 		function(){
 			$(this).find('ul').toggle();
 		}
 	);
-
-	$('.skins').on('click', function(){
-		$('#J_Skins').attr('href', '__PUBLIC__/Demo/skins/light/style.css');
-	});
 	
 	// 弹出页
 	$(".newWin").on('click', function() {
 		window.open($(this).attr('url'));
 	});
 	
-	// 直达框
-	$(document).on('click', function(){
-		$('#direct_text').val($('#direct_text').attr('txt')).removeClass('ipton');
-	});
-	$("#header").on('mouseenter', function(){
-		var tag = $.trim($('#direct_text').val());
-		if(tag == $('#direct_text').attr('txt')){
-			$("#direct_text").select().addClass('ipton');
-		}else{
-			$("#direct_text").addClass('ipton');
-		}
-	}).on('mouseleave', function(){
-		var tag = $.trim($('#direct_text').val());
-		if(tag == '' || tag == $('#direct_text').attr('txt')){
-			$('#search_text').select();
-			$('#direct_text').removeClass('ipton');
-		}
-	});
-	$("#direct_text").on('click', function(){
-		var tag = $.trim($('#direct_text').val());
-		if (tag == $('#direct_text').attr('txt')){
-			$('#direct_text').val('').addClass('ipton');
-		}
-		return false;
-	});
-
-	$('.J_direct_submit').on('click', function(){
-		$("#frm_drct").trigger('submit');
-		$("#direct_text")[0].focus();
-		return false;
-	});
-	$("#frm_drct").on('submit', function(){
-		var tag = $.trim($('#direct_text').val());
-		if (tag == '' || tag == $('#direct_text').attr('txt')){
-			return false;
-		}
-		$('#direct_text').select();
-	});
-	
-	//保存自留地网址
-	$('.J_myarea_web_save').click(function() {
-		var id = $('#J_myarea_id').val();
-		var web_name= $('#J_myarea_web_name').val();
-		var url= $('#J_myarea_web_url').val();
-		var t = getLength(url);
-
-		$('#J_myarea_web_name').trigger('keyup');
-
-		if(t == 0){
-			$('#J_myarea_tip').text('链接为空!').css('color', '#f00');
-			return false;
-		}
-
-		$.post(URL + "/updateArealist", {
-				id: id,
-				web_name: web_name,
-				url: url
-			},
-			function(data) {
-				if (data.indexOf("updateOK") >= 0) {
-					var myarea_web_obj = $('.J_myarea_div ul li[id="'+id+'"] span');
-					$('.J_myarea_div ul li[id="'+id+'"] span b').text(web_name);
-					var new_url = myarea_web_obj.attr('url').replace(myarea_web_obj.attr('data-url'), url);
-					myarea_web_obj.attr('url', new_url);
-					myarea_web_obj.attr('data-url', url);
-					$('#J_myarea_tip').text('保存成功!').css('color', '#d20015');
-					$('#J_myarea_tip').show();
-
-				} else {
-					$('#J_myarea_tip').text('保存失败!').css('color', '#f00');
-					$('#J_myarea_tip').show();
-				}
-				setTimeout(function(){
-					$('#J_myarea_tip').hide();
-					$('.J_zld_edit_box').hide();
-				}, 2000);
-		});
-	});
-
-	$('#J_myarea_web_url').keypress(function(event) {
-		if (event.keyCode == 13) {
-			$(".J_myarea_web_save").trigger("click");
-			return false;
-		}
-	});
-	
 });
 
+var ZhiDaLan = { // 直达框
+	Init: function(){
+		$(document).on('click', function(){
+			$('#direct_text').val($('#direct_text').attr('txt')).addClass('ipton');
+		});
+
+		$("#header").on('mouseenter', function(){
+			var tag = $.trim($('#direct_text').val());
+			if(tag == $('#direct_text').attr('txt')){
+				$("#direct_text").select().removeClass('ipton');
+			}else{
+				$("#direct_text").removeClass('ipton');
+			}
+		}).on('mouseleave', function(){
+			var tag = $.trim($('#direct_text').val());
+			if(tag == '' || tag == $('#direct_text').attr('txt')){
+				$('#search_text').select();
+				$('#direct_text').addClass('ipton');
+			}
+		});
+
+		$("#direct_text").on('click', function(){
+			var tag = $.trim($('#direct_text').val());
+			if (tag == $('#direct_text').attr('txt')){
+				$('#direct_text').val('').removeClass('ipton');
+			}
+			return false;
+		}).on('blur', function(){
+			$('#direct_text').addClass('ipton');
+		});
+
+		$('.J_direct_submit').on('click', function(){
+			$("#frm_drct").trigger('submit');
+			$("#direct_text")[0].focus();
+			return false;
+		});
+
+		$("#frm_drct").on('submit', function(){
+			var tag = $.trim($('#direct_text').val());
+			if (tag == '' || tag == $('#direct_text').attr('txt')){
+				return false;
+			}
+			$('#direct_text').select();
+		});
+	}
+};
+
+var Zld = { // 自留地
+	IsSortable: false, //是否为拖拽点击，true则不打开自留地网址
+	Init: function(){
+		var self = this;
+		var obj = $('#J_ZldList');
+
+		$(document).on('click', '#J_ZldList .add', function(){
+			if(self.CheckLogin()){
+				self.Create();
+			}
+		});
+		$(document).on('click',  '#J_ZldList .ctl', function(){
+			if(self.CheckLogin()){
+				if($(this).hasClass('add')){ return false; }
+				var o = $(this).closest('li');
+				var id = o.data('id');
+				var nm = o.find('b').html();
+				var url = o.data('url');
+				self.Create(id, nm, url);
+				return false;
+			}
+		});
+		$(document).on('click', '#J_ZldList .nm', function(){
+			if (!Zld.IsSortable) {
+				var o = $(this).closest('li');
+				var url = o.data('url');
+				self.Go(url);
+			} else {
+				Zld.IsSortable = false;
+			}
+			return false;
+		});
+
+		$('#J_sortable').sortable({
+			update: function(event, ui){
+				Zld.IsSortable = true;
+				
+				$.post(
+					URL + '/sortArealist', 
+					{'area' : $(this).sortable('toArray')},
+					function(data) {
+						if (data == 1) {
+							//成功
+						} else if (data == 0){
+							//失败
+						} else {
+							//失败
+						}
+					}
+				);
+			}  
+		});
+		$('#J_sortable').sortable('enable');
+		
+		$(document).on('click', '#J_Zld .lkd-add, #J_Zld .lkd-edit', function(){
+
+			var o = $('#J_Zld');
+
+			var id = o.find('input[name="id"]').val();
+			var name = o.find('input[name="name"]').val();
+			var url = o.find('input[name="url"]').val();
+
+			if (!name) {
+				alert("请输入网站名称");
+				return false;
+			}
+			if (!url) {
+				alert("请输入网址");
+				return false;
+			}
+			
+			$.post(
+				URL + '/updateArea', 
+				{ 'web_id': id, 'web_url': url, 'web_name': name },
+				function(data) {
+					var licur = function(){
+						var li = null;
+						obj.find('ul>li').each(function(){
+							if($(this).data('id') == id){
+								li = $(this);
+								return;
+							}
+						});
+						return li;
+					}
+					if(data == 1){ //更新成功
+						var li = licur();
+						li.attr('url', '/Link/index.html?mod=myarea&amp;url=' + url);
+						li.data('url', url);
+						li.find('b').html(name);
+					}else if(data > 1){ //新加成功
+						var li = obj.find('.add').closest('li');
+						li.before(self.CreateItem(data, name, url));
+					}else if(data == -1){
+						User.Login('请先登录');
+					}else{
+						alert('操作失败');
+					}
+					o.dialog('close');
+				}
+			);
+			return false;
+		});
+		
+		$(document).on('click', '#J_Zld .lkd-del', function(){
+
+			var o = $('#J_Zld');
+			var id = o.find('input[name="id"]').val();
+			
+			$.post(
+				URL + '/delArea', 
+				{ 'web_id': id },
+				function(data) {
+					var licur = function(){
+						var li = null;
+						obj.find('ul>li').each(function(){
+							if($(this).data('id') == id){
+								li = $(this);
+								return;
+							}
+						});
+						return li;
+					}
+					if(data == 1){
+						var li = licur();
+						li.remove();
+					}else if(data == -1){
+						User.Login('请先登录');
+					}else{
+						alert('操作失败');
+					}
+					o.dialog('close');
+				}
+			);
+			return false;
+		});
+	},
+	CheckLogin: function(){
+		if($CONFIG.IsLogin){
+			User.Login('请先登录');
+			return false;
+		}
+		return true;
+	},
+	Go: function(url){
+		var obj = $('#J_MyAreaForm');
+		obj.find('input[name="url"]').val(url);
+		obj.submit();
+	},
+	Create: function(id, nm, url){
+		if(!$('#J_Zld').size()){
+			var hl = '';
+			hl = hl + '<div class="lk-dialog lk-dialog-zld" id="J_Zld">';
+			hl = hl + '	<div class="lkd-hd">';
+			hl = hl + '		<a class="close" href="javascript:;">X</a>';
+			hl = hl + '	</div>';
+			hl = hl + '	<div class="lkd-bd">';
+			hl = hl + '		<form action="">';
+			hl = hl + '			<ul>';
+			hl = hl + '				<li><input class="ipt" type="text" name="name" placeholder="网站名称" /></li>';
+			hl = hl + '				<li><input class="ipt" type="text" name="url" placeholder="网址" /></li>';
+			hl = hl + '			</ul>';
+			hl = hl + '		</form>';
+			hl = hl + '	</div>';
+			hl = hl + '	<div class="lkd-ft">';
+			hl = hl + '		<input type="hidden" name="id" value="" />';
+			hl = hl + '		<span class="editp" style="display:none;"><a class="lkd-edit" href="javascript:;">确认编辑</a>';
+			hl = hl + '		<a class="lkd-del" href="javascript:;">删除</a></span>';
+			hl = hl + '		<span class="addp"><a class="lkd-add" href="javascript:;">确认添加</a></span>';
+			hl = hl + '	</div>';
+			hl = hl + '</div>';
+			$('body').append(hl);
+
+			var obj = $('#J_Zld');
+
+			obj.dialog({
+				autoOpen: false,
+				width: 384,
+				modal: true,
+				resizable: false,
+				open: function(){
+					setTimeout(function(){obj.find('input[name="name"]').select();}, 20);
+				}
+			});
+
+			obj.find('.close').on('click', function(){
+				obj.dialog('close');
+				return false;
+			});
+
+			obj.find('input[type="text"]').on('focus', function(){
+				$(this).css('background', '#fff');
+			}).on('blur', function(){
+				$(this).css('background', '#eeefef');
+			});
+
+			obj.find('input[name="name"],input[name="url"]').on('keydown', function(event){
+				if (event.keyCode == 13) {
+					if(obj.find('.editp').is(":visible")){
+						obj.find('.lkd-edit').trigger('click');	
+					}else{
+						obj.find('.lkd-add').trigger('click');
+					}
+				}
+			});
+		}
+
+		var obj = $('#J_Zld');		
+		if(id){
+			obj.find('input[name="id"]').val(id);
+			obj.find('input[name="name"]').val(nm);
+			obj.find('input[name="url"]').val(url);
+			obj.find('.editp').show();
+			obj.find('.addp').hide();
+		}else{
+			obj.find('input[name="id"]').val('');
+			obj.find('input[name="name"]').val('');
+			obj.find('input[name="url"]').val('');
+			obj.find('.editp').hide();
+			obj.find('.addp').show();
+		}
+
+		obj.dialog('open');
+	},
+	CreateItem: function(id, nm, url){
+		var hl = '<li id="'+ id +'" url="/Link/index.html?mod=myarea&amp;url='+ url +'" data-id="'+ id +'" data-url="'+ url +'">';
+		hl = hl + '<span class="nm"><i class="mask"></i><b>'+ nm +'</b></span>';
+		hl = hl + '<span class="ctl"><i class="mask"></i></span>';
+		hl = hl + '</li>';
+		return hl;
+	}
+};
