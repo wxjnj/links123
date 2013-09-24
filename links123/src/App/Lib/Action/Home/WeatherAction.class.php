654e7ac1533db18213e3c6d4dcc08ba42633336a
ydx <x6doooo@gmail.com>
2013-09-24 23:58:43 +0800

    safari天气控件不显示fixed

--- links123/src/App/Lib/Action/Home/WeatherAction.class.php
+++ links123/src/App/Lib/Action/Home/WeatherAction.class.php
@@ -8,6 +8,61 @@
  */
 import("@.Common.CommonAction");
 class WeatherAction extends CommonAction {
+
+	public function index_new(){
+		if(!empty($_SERVER["HTTP_CLIENT_IP"]))  
+   		$cip = $_SERVER["HTTP_CLIENT_IP"];  
+		else if(!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))  
+   		$cip = $_SERVER["HTTP_X_FORWARDED_FOR"];  
+		else if(!empty($_SERVER["REMOTE_ADDR"]))  
+   		$cip = $_SERVER["REMOTE_ADDR"];  
+		else  
+   		$cip = "无法获取！";  
+		$region = file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip=182.92.247.2');
+		$this->assign('region', $region->data->city);
+		$this->display();
+	}
+
+	public function city(){
+		if(isset($_GET['city'])){
+			$szUrl = 'http://ext.weather.com.cn/city?'.$_GET['city'];
+			$UserAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506; .NET CLR 3.5.21022; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $szUrl);
+			curl_setopt($curl, CURLOPT_REFERER, 'http://ext.weather.com.cn/');
+			curl_setopt($curl, CURLOPT_HEADER, 0);  //0表示不输出Header，1表示输出
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($curl, CURLOPT_ENCODING, '');
+			curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
+			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+			$data = curl_exec($curl); 
+			$this->assign('city', $data);
+		}
+		$this->display();
+	}
+
+	public function data(){
+		if(isset($_GET['id'])){
+			$region = $_GET['id'];
+			$szUrl = 'http://ext.weather.com.cn/' . $region . '.json';
+			$UserAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506; .NET CLR 3.5.21022; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $szUrl);
+			curl_setopt($curl, CURLOPT_REFERER, 'http://ext.weather.com.cn/');
+			curl_setopt($curl, CURLOPT_HEADER, 0);  //0表示不输出Header，1表示输出
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($curl, CURLOPT_ENCODING, '');
+			curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
+			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+			$data = curl_exec($curl); 
+			$this->assign('weather_json', $data);
+		}
+		$this->display();
+	}
 	
 	public function index() {
 		$ip = get_client_ip();
