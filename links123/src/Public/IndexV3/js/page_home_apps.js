ccb2d666fb101841d0355d1485aadcbcf1a2a642
lpgray <rayzy1991@gmail.com>
2013-09-25 17:20:05 +0800

    将正在修改的v3首页单备份一份进行开发，省的不停的跟其他人冲突

--- links123/src/Public/IndexV3/js/page_home_apps.js
+++ links123/src/Public/IndexV3/js/page_home_apps.js
@@ -4,63 +4,63 @@
  * @datetime: 2013-09-25 13:05
  */
 $(function($){
-  // /*
-   // * app开关触发器
-   // */
-  // $.fn.links123_apptrigers = function( selector ){
-    // this.delegate(selector, 'click', function(){
-      // var appId = $(this).data('app-id');
-      // $(this).data('links_app') || $(this).data('links_app', new App(appId));
-      // var app = $(this).data('links_app');
-      // app.show();
-    // });
-  // }
-  // $('#J_Apps').links123_apptrigers('li');
-//   
-  // /*
-   // * App类
-   // */
-  // var App = function( appId ){
-    // this.appId = appId;
-    // this.$elem = $( '#' + appId );
-    // //console.debug('init app-' + appId + '...');
-  // };
-  // App.prototype = {
-    // show : function(){
-      // this.$elem.show();
-      // //console.debug( 'app-' + this.appId + ' showing...' );
-    // },
-    // close : function(){
-      // this.$elem.hide();
-    // }
-  // };
-//   
-  // /*
-   // * App管理器对象
-   // */
-  // var AppMgr = {
-    // currentApp : null
-  // }
+  /*
+   * app开关触发器
+   */
+  $.fn.links123_apptrigers = function( selector ){
+    this.delegate(selector, 'click', function(){
+      var appId = $(this).attr('href');
+      $(this).data('links_app') || $(this).data('links_app', new App(appId));
+      var app = $(this).data('links_app');
+      app.show();
+    });
+  }
+  $('#J_Apps').links123_apptrigers('a');
+  
+  /*
+   * App类
+   */
+  var App = function( appId ){
+    this.appId = appId;
+    this.$elem = $( appId );
+    //console.debug('init app-' + appId + '...');
+  };
+  App.prototype = {
+    show : function(){
+      this.$elem.show();
+      console.debug( 'app-' + this.appId + ' showing...' );
+    },
+    close : function(){
+      this.$elem.hide();
+    }
+  };
+  
+  /*
+   * App管理器对象
+   */
+  var AppMgr = {
+    currentApp : null
+  }
   
   //使用FancyBox插件显示
-  $('#J_Apps').find('.fancybox-btn').fancybox({
-      padding: 10,
-      openSpeed  : 150,
-      closeSpeed  : 150,
-      closeClick : true,
-      helpers : {
-        overlay : null,
-        
-      },
-      onComplete : function(){
-        console.debug('...');
-      }
-  });
+  // $('#J_Apps').find('.fancybox-btn').fancybox({
+      // padding: 10,
+      // openSpeed  : 150,
+      // closeSpeed  : 150,
+      // closeClick : true,
+      // helpers : {
+        // overlay : null,
+//         
+      // },
+      // onComplete : function(){
+        // console.debug('...');
+      // }
+  // });
   
   //可拖动的panel用jquery.ui显示
-  $('#J_Apps').find('.jquery-ui-panel').fancybox({
-    
-  });
+  // $('#J_Apps').find('.jquery-ui-panel').fancybox({
+//     
+  // });
   
   
   
