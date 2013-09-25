e72d77a9513621436b3967b1b91b1511d2018a8c
lpgray <rayzy1991@gmail.com>
2013-09-25 23:49:23 +0800

    重写v3首页便签小组件，进行app模块重构（完成度75%），将正在编写的代码以_wip为后缀。暂时不影响测试环境

--- links123/src/Public/IndexV3/js/page_home_apps.js
+++ links123/src/Public/IndexV3/js/page_home_apps.js
@@ -3,87 +3,112 @@
  * @author: lpgray
  * @datetime: 2013-09-25 13:05
  */
-$(function($){
-  /*
-   * app开关触发器
-   */
-  $.fn.links123_apptrigers = function( selector ){
-    this.delegate(selector, 'click', function(){
-      var appId = $(this).attr('href');
-      $(this).data('links_app') || $(this).data('links_app', new App(appId));
-      var app = $(this).data('links_app');
-      app.show();
-    });
-  }
-  $('#J_Apps').links123_apptrigers('a');
-  
-  /*
-   * App类
-   */
-  var App = function( appId ){
-    this.appId = appId;
-    this.$elem = $( appId );
-    //console.debug('init app-' + appId + '...');
-  };
-  App.prototype = {
-    show : function(){
-      this.$elem.show();
-      console.debug( 'app-' + this.appId + ' showing...' );
-    },
-    close : function(){
-      this.$elem.hide();
-    }
-  };
-  
-  /*
-   * App管理器对象
-   */
-  var AppMgr = {
-    currentApp : null
-  }
-  
-  //使用FancyBox插件显示
-  // $('#J_Apps').find('.fancybox-btn').fancybox({
-      // padding: 10,
-      // openSpeed  : 150,
-      // closeSpeed  : 150,
-      // closeClick : true,
-      // helpers : {
-        // overlay : null,
-//         
-      // },
-      // onComplete : function(){
-        // console.debug('...');
-      // }
-  // });
-  
-  //可拖动的panel用jquery.ui显示
-  // $('#J_Apps').find('.jquery-ui-panel').fancybox({
-//     
-  // });
-  
-  
-  
-  /** $日历 * */
-  // $('#J_calendar').click(function(){
-    // $('#J_calendar_iframe').attr('src', 'http://baidu365.duapp.com/wnl.html');
-    // $.fancybox({
-      // href: '#J_box_calendar',
-      // //closeBtn : false,
-      // helpers:  {
-        // title:  null,
-        // overlay : null
-      // },
-      // width: 550,
-      // height: 525,
-      // autoSize: false
-    // });
-    // return false;
-  // });
-  // $('#J_box_calendar_list a').click(function() {
-    // $('#J_calendar_iframe').attr('src', $(this).attr('data-url'));
-    // return false;
-  // });
-  /** 日历$ **/
-  
-}(jQuery));
+$( function($) {
+	/*
+	 * app开关触发器
+	 */
+	$.fn.links123_apptrigers = function(selector) {
+		this.delegate(selector, 'click', function() {
+			var appId = $(this).attr('href');
+			$(this).data('links_app') || $(this).data('links_app', new App(appId));
+			var app = $(this).data('links_app');
+			app.show();
+			return false;
+		});
+	}
+	$('#J_Apps').links123_apptrigers('a');
+	
+	/*
+	 * App类
+	 */
+	var App = function(appId) {
+		this.appId = appId;
+		this.$elem = $(appId);
+		this.$elem.addClass('links123-app-frame');
+		this.initStyle();
+		this.bindEvent();
+		callbacks[appId] && callbacks[appId]( this );
+	};
+	App.prototype = {
+		show : function() {
+			this.$elem.fadeIn();
+		},
+		close : function() {
+			this.$elem.hide();
+		},
+		initStyle : function() {
+			this.w = this.$elem.outerWidth();
+			var h = this.$elem.outerHeight();
+			var fixedStyle = {
+				'margin-left' : -this.w/2,
+				'margin-top' : -h/2 + $('#header').outerHeight()
+			}
+			this.$elem.css(fixedStyle);
+		},
+		bindEvent : function(){
+			var $self = this;
+			$self.$elem.prepend('<div class="links123-close-wrap" style="width:'+this.w+'"><a href="#">x</a></div>');
+			$self.$elem.children('.links123-close-wrap').delegate('a', 'click', function(){
+				$self.close();
+			});
+			$(document).bind('keyup', function( e ){
+				e.keyCode === 27 && $self.close();
+			});
+		},
+		clone : function(){
+			var $cloneObj = this.$elem.clone();
+			$cloneObj.attr('id', this.appId.substring(1) + 'x');
+			$('body').append( $cloneObj );
+			return new App( '#' + $cloneObj.attr('id') );
+		},
+		sync : function(){
+			$.post('', this.data, function(){}, 'json');
+		}
+	};
+	
+	/*
+	 * callbacks
+	 * 不同app初始化会调用这里的callback
+	 * 以dom id为key调用对应的函数，每个函数欧诺只会调用一次，而且是按需调用
+	 */
+	var callbacks = {
+		/*
+		 * 便签
+		 */
+		'#J_box_note' : function( app ){
+			var $note = app.$elem;
+			var $textarea = $note.find('textarea');
+			var textareaBg = null;
+			var content = null;
+			var app = app;
+			load();
+			// 变背景色
+			$note.delegate('[class^=color_]', 'click', function(){
+				textareaBg = '' + $(this).css('background-color');
+				$textarea.css('background', textareaBg );
+				remember();
+			});
+			// 新建
+			$note.delegate('.btn_add', 'click', function(){
+				var back = app.clone();// clone a new note app object
+				callbacks['#J_box_note'](back);
+			});
+			// 删除
+			$note.delegate('.btn_clear', 'click', function(){
+				$textarea.val('');
+				remember();
+			});
+			// remember
+			function remember(){
+				$.cookies.set('links123_note_bg', textareaBg);
+				$.cookies.set('links123_note_content', $textarea.val());
+			}
+			// load
+			function load(){
+				$textarea.css('background', $.cookies.get('links123_note_bg') );
+				$.cookies.get('links123_note_content') && $textarea.val( $.cookies.get('links123_note_content') );
+			}
+		}
+	}
+
+}(jQuery)); 
