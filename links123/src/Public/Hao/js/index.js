23d2919fd90e27343b9f7612cffe466989661ef2
ydx <x6doooo@gmail.com>
2013-10-03 14:36:37 +0800

    新导航页增加瀑布加载限制、页码、回到顶部按钮

--- links123/src/Public/Hao/js/index.js
+++ links123/src/Public/Hao/js/index.js
@@ -1,4 +1,6 @@
 $(function(){
+
+	//瀑布
 	var gutterWidth = $('body').hasClass('widescreen') ? 8 : 15;
 	$('#K_waterfall').waterfall({
 		itemCls: 'item-box',
@@ -8,9 +10,54 @@ $(function(){
 		gutterWidth: gutterWidth,
 		gutterHeight: 15,
 		checkImagesLoaded: false,
+		maxPage: 3,
+		resizable: false,
 		path: function(page) {
 			return $CONFIG.PUBLIC + '/Hao/js/data.json?page=' + page;
-		}
+		},
+		callbacks: {
+        	loadingFinished: function($loading, isBeyondMaxPage) {
+            	if ( !isBeyondMaxPage ) {
+                	$loading.fadeOut();
+            	} else {
+                	$loading.hide();
+                	$('.pagination, .footer').show();
+            	}
+        	}
+    	}
 	});
+
+	//分页
+	$('#pagination').pagination({
+    	total: 90,     // 总页数
+    	current: 1   // 当前所在页
+  	});
+
+  	$('#pagination').on('click', 'a', function(){
+    	var self = $(this);
+    	var num;
+    	if(self.parent().hasClass('disabled') || 
+      		self.parent().hasClass('active')) return;
+    	num = self.attr('data-num');
+    	$('#pagination').pagination({
+      		current: num
+    	});
+  	});
+
+	//回到页顶
+	function gototop(){
+		var y = document.documentElement.scrollTop || document.body.scrollTop;
+		window.scrollTo(0, y / 1.1);
+		if(y > 0){
+			setTimeout(gototop, 5);
+		}else{
+			return;
+		}
+	}
+	$('#K_top_btn').on('click', function(){
+		gototop();
+	});
+
+
 });
 
