c768cd3cba587de76ff4115be38a10333ed2980f
slate <itphper@qq.com>
2013-10-06 18:55:19 +0800

    *  测试站去除301重定向

--- links123/src/App/Conf/English/config.php
+++ links123/src/App/Conf/English/config.php
@@ -6,6 +6,6 @@ return array(
     'ENGLISH_DICT_SPEAK_URL'  => U('English/Index/getAudio',array('word'=>'###','type'=>'1')), //英语角单词读音获取地址###为占位符
     //'ENGLISH_DICT_SPEAK_URL' => 'http://dict.youdao.com/dictvoice?audio=###&type=1', //英语角单词读音获取地址###为占位符
     'ENGLISH_SPEECH_API_URL'  => 'http://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US&maxresults=5', //英语角语音识别API地址
-    'ENGLISH_REDIRECT_URL' => 'http://en.links123.cn/',	//英语角重定向地址
+   // 'ENGLISH_REDIRECT_URL' => 'http://en.links123.cn/',	//英语角重定向地址
 );
 ?>
