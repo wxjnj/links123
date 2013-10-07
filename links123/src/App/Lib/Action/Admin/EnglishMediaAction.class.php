3b2b42d6556561989f28e812ed8bcc086f40d6dd
slate <itphper@qq.com>
2013-10-07 19:40:02 +0800

    * 添加视频自动解析

--- links123/src/App/Lib/Action/Admin/EnglishMediaAction.class.php
+++ links123/src/App/Lib/Action/Admin/EnglishMediaAction.class.php
@@ -234,10 +234,16 @@ class EnglishMediaAction extends CommonAction {
             }
             $model->ted = intval($tedId);
         }
+        $media['media_id'] = $model->id;
+        $media['media_source_url'] = $model->media_source_url;
         //保存当前数据对象
         $list = $model->add();
         if ($list !== false) { //保存成功
             $model->commit();
+            
+            //TODO 推荐视频解析 @author slate date 20131001
+            $model->analysisMediaPlayCode($media);
+            
             $this->success('新增成功!', cookie('_currentUrl_'));
         } else {
             $model->rollback();
@@ -328,10 +334,19 @@ class EnglishMediaAction extends CommonAction {
                 $model->ted = $tedId;
             }
         }
+        $media['media_id'] = $model->id;
+        $media['media_source_url'] = $model->media_source_url;
         // 更新数据
         $list = $model->save();
         if (false !== $list) {
             $model->commit();
+            
+            //TODO 推荐视频解析 @author slate date 20131001
+            if (!$model->play_code) {
+	            
+	            $model->analysisMediaPlayCode($media);
+            }
+            
             //成功提示
             $this->success('编辑成功!', cookie('_currentUrl_'));
         } else {
