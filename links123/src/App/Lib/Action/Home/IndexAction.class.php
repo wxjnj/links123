d958fe215f07c3637920c36a580cf81cae03f7c7
ydx <x6doooo@gmail.com>
2013-10-13 19:45:03 +0800

    切换skin & 切换主题

--- links123/src/App/Lib/Action/Home/IndexAction.class.php
+++ links123/src/App/Lib/Action/Home/IndexAction.class.php
@@ -65,6 +65,10 @@ class IndexAction extends CommonAction {
 		$this->assign("skinList", $skins['list']);
 		$this->assign("skinCategory", $skins['category']);
 
+		if(!$themeId && $themeId > 2) { //暂时只有1和2
+			$themeId = 1;
+		}
+
 		$this->assign('themeId', $themeId);
 
 		$app_list = $this->getApps($_SESSION['app_sort']);
