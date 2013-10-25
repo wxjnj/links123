7a1ee30fb66552b8754de22c4205b983e5e972d9
reasonpun <reasonpun@links123.cn>
2013-10-25 11:23:56 +0800

    添加了清除缓存操作

--- links123/src/App/Lib/Action/Admin/EnglishLevelnameAction.class.php
+++ links123/src/App/Lib/Action/Admin/EnglishLevelnameAction.class.php
@@ -3,93 +3,111 @@
 * 英语角Action
 * author reasono
 */
+import("App.Logic.Admin.EnglishLevelnameLogic");
 class EnglishLevelnameAction extends CommonAction {
+    protected $cEnglishLevelnameLogic = null;
 
-	public function _filter(&$map, &$param) {
-        if (isset($_REQUEST['name'])) {
-            $name = $_REQUEST['name'];
-        }
-        if (!empty($name)) {
-            $map['name'] = array('like', "%" . $name . "%");
-        }
-        $this->assign('name', $name);
-        $param['name'] = $name;
-        
-        $this->assign("param", $param);
+    public function _initialize() {
+        $this->cEnglishLevelnameLogic = new EnglishLevelnameLogic();
+        parent::_initialize();
     }
 
-	//
+    protected function _filter(&$map, &$param) {
+
+    }
+
     public function index() {
-        //列表过滤器，生成查询Map对象
-        $map = array();
-        $param = array();
-        if (method_exists($this, '_filter')) {
-            $this->_filter($map, $param);
-        }
-        $type = intval($_GET['type']) > 0 ? intval($_GET['type']) : 1;
-        $map['level'] = $type;
-        $name = $this->getActionName();
-        $model = D($name);
-        if (!empty($model)) {
-            $this->_list($model, $map, $param, 'id', false);
-        }
-        $this->assign("type", $type);
+        $type  = isset($_REQUEST["type"])  ? intval($_REQUEST["type"])  : 1; 
+        $title = isset($_REQUEST["title"]) ? $_REQUEST["title"] : ""; 
+
+        $category = $this->cEnglishLevelnameLogic->getCategoryLevelListBy($type, $title);
+        $this->assign('category', $category);
+        $this->assign('type', $type);
+        $this->assign('title', $title);
         $this->display();
         return;
     }
 
-	public function add() {
-		$this->assign('type', $_GET['type']);
-    	$this->display();
-    	return;
-	}
+    public function add() {
+        $this->assign('type', $_GET['type']);
+        $this->display();
+        return;
+    }
 
-	public function edit() {
-        $cate = D("EnglishLevelname")->getInfoById($_GET['id']);
-		$this->assign('category', $cate);
-		$this->assign('type', $_GET['type']);
-    	$this->display();
-    	return;
-	}
+    public function edit() {
+        $cate = $this->cEnglishLevelnameLogic->getCategoryInfoById($_GET['id']);
+        $this->assign('category', $cate[0]);
+        $this->assign('type', $_GET['type']);
+        $this->display();
+        return;
+    }
 
-	public function insert() {
-		$model = D("EnglishLevelname");
-		if (false === $model->create()) {
+    public function insert() {
+        $model = D("EnglishLevelname");
+        if (false === $model->create()) {
             $this->error($model->getError());
         }
         $model->created = time();
         //保存当前数据对象
         $list = $model->add();
         if ($list !== false) { //保存成功
+            $this->flushCache();
             $this->success('新增成功!', cookie('_currentUrl_'));
         } else {
             //失败提示
             $this->error('新增失败!');
         }
-	}
+    }
 
-     // 排序
-    public function sort(){
-    	$model = D("EnglishLevelname");
-    	$map = array();
-    	if (!empty($_GET['sortId'])) {
-    		$map['id'] = array('in', $_GET['sortId']);
-    	}
-    	else {
-    		$params = explode("&", $_SESSION[C('SEARCH_PARAMS_KEY')]);
-    		foreach ($params as &$value) {
-    			$temp = explode("=", $value);
-    			if ( !empty($temp[1]) && $temp[0] != 'sort' && $temp[0] != 'order' ) {
-    				$map[$temp[0]] = $temp[1];
-    			}
-    		}
-    	}
-    	$sortList = $model->where($map)->order('sort asc')->select();
-    	foreach ($sortList as $key=>$value) {
-    		$sortList[$key]['txt_show'] = $value['name'];
-    	}
-    	$this->assign("sortList", $sortList);
-    	$this->display("../Public/sort");
-    	return;
+    public function forbid() {
+        $ret = $this->cEnglishLevelnameLogic->forbidCategoryById($_GET['id']);
+        if ($ret !== false) {
+            $this->flushCache();
+            $this->success('状态禁用成功', __URL__ . "/index/type/" . $_GET['type']);
+        } else {
+            $this->error('状态禁用失败！');
+        }
+    }
+
+    public function resume() {
+        $ret = $this->cEnglishLevelnameLogic->resumeCategoryById($_GET['id']);
+        if ($ret !== false) {
+            $this->flushCache();
+            $this->success('状态恢复成功', __URL__ . "/index/type/" . $_GET['type']);
+        } else {
+            $this->error('状态恢复失败！');
+        }
+    }
+
+    public function update() {
+        $category["id"]     = intval($_REQUEST["id"]);
+        $category["name"]   = $_REQUEST["name"];
+        $category["level"]  = $_REQUEST["level"];
+        $category["sort"]   = $_REQUEST["sort"];
+        $category["status"] = $_REQUEST["status"];
+        $ret = $this->cEnglishLevelnameLogic->updateCatetory($category);
+        if ($ret !== false) {
+            $this->flushCache();
+            $this->success('更新成功', __URL__ . "/index/type/" . $_REQUEST['type']);
+        } else {
+            $this->error('更新失败！');
+        }
+    }
+
+    public function foreverdelete() {
+        $ret = $this->cEnglishLevelnameLogic->deleteCategory($_REQUEST["id"]);
+        if ($ret !== false) {
+            $this->flushCache();
+            $this->success('删除成功', __URL__ . "/index/type/" . $_REQUEST['type']);
+        } else {
+            $this->error('删除失败！');
+        }
+    }
+
+    private function flushCache() {
+        
+        $cache = Cache::getInstance('Redis');
+        $cache->clear();
+
     }
 }
