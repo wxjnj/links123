60b4901d1cca8ea982c737ea23774540619c0389
ydx <x6doooo@gmail.com>
2013-11-08 20:48:37 +0800

    修正zld自适应算法

--- linksFrontEnd/src/v303b/js/index.js
+++ linksFrontEnd/src/v303b/js/index.js
@@ -178,7 +178,7 @@ var Zld = { // 自留地
 		if (boxWidth - fstLineWidth > 55) {
 			var w = lis.eq(overIndex).width() + 5 - (boxWidth - fstLineWidth);
 			var xw = Math.floor(w / (overIndex + 1) / 2);
-			var diff = w - xw * (overIndex + 1) * 2;
+			var diff = w - xw * (overIndex + 1) * 2 + 1;
 
 			var s = lis.filter(':lt(' + (overIndex + 1) + ')');
 			$.each(s, function(k, v) {
@@ -203,7 +203,7 @@ var Zld = { // 自留地
 			// 差距过小，使用本行增加宽度适应行宽
 			var w = boxWidth - fstLineWidth;
 			var xw = Math.floor(w / (overIndex) / 2);
-			var diff = w - xw * 2 * overIndex;
+			var diff = w - xw * 2 * overIndex - 1;
 
 			var s = lis.filter(':lt(' + overIndex + ')');
 			$.each(s, function(k, v){
