973ad613d0840111cae6d6210ea23f91a6f56e2e
yuanyang <itphper@qq.com>
2013-11-09 23:10:14 +0800

    * 个人中心设置模块

---
+++ links123/src/App/Lib/Action/User/SetAction.class.php
@@ -0,0 +1,99 @@
+<?php 
+
+/**
+ * @desc 个人中心设置模块
+ * 
+ * @author slate date:2013-10-30
+ */
+import("@.Common.CommonAction");
+class SetAction extends CommonAction
+{
+	/**
+	 * @desc 个人中心
+	 * 
+	 * @param 
+	 * 
+	 * @return 
+	 * 
+	 * @author slate date:2013-10-30
+	 */
+	public function index() {
+		
+		$this->display();
+	}
+	
+	public function avator() {
+		
+		$this->display();
+	}
+	
+	public function profile() {
+	
+		$this->display();
+	}
+	
+	public function edu() {
+	
+		$this->display();
+	}
+	
+	public function tag() {
+	
+		$this->display();
+	}
+	
+	public function domain() {
+	
+		$this->display();
+	}
+	
+	public function safe() {
+	
+		$this->display();
+	}
+	
+	public function pwd() {
+	
+		$this->display();
+	}
+	
+	public function pwdprotect() {
+	
+		$this->display();
+	}
+	
+	public function find() {
+	
+		$this->display();
+	}
+	
+	public function phone() {
+	
+		$this->display();
+	}
+	
+	public function mail() {
+	
+		$this->display();
+	}
+	
+	public function common() {
+	
+		$this->display();
+	}
+	
+	public function record() {
+	
+		$this->display();
+	}
+	
+	public function data() {
+	
+		$this->display();
+	}
+	
+	public function stop() {
+	
+		$this->display();
+	}
+}
