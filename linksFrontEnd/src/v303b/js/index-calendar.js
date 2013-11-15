9b70e235d4cb27731c9f33a826da7cba2eccf424
jack <19258390@qq.com>
2013-11-15 22:30:16 +0800

    日程表刷新后引号问题

--- linksFrontEnd/src/v303b/js/index-calendar.js
+++ linksFrontEnd/src/v303b/js/index-calendar.js
@@ -551,8 +551,8 @@ $(function(){
             }else{
                 short_desc = desc;
             }
-
-            self.element.find('.desc').html('<a class="desc-content ' + color + '" data-color="' + color + '" title="' + escape(short_desc) + '" href="javascript:;">' + unescape(short_desc) + '</a>');
+            short_desc = $('<div/>').html(short_desc).text(); //后台传输过来的数据会编码html，利用这个解码
+            self.element.find('.desc').html('<a class="desc-content ' + color + '" data-color="' + color + '" title="' + escape(short_desc) + '" href="javascript:;">' + short_desc + '</a>');
             self.element.append('<a class="delete-btn" href="javascript:;">×</a>');
             self.element.attr('data-id', id);
         },
