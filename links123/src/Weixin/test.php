3d4e8910500a1b198dacc4daa892b42c37f920ba
spring <spring@links123.cn>
2013-11-22 10:32:36 +0800

    merge

---
+++ links123/src/Weixin/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 13-11-22
+ * Time: 上午9:11
+ */
+include "Api.class.php";
+
+
+$matches=array();
+if(preg_match("/天气@(\W+)/","天气@上海",$matches)){
+   // print_r($matches);
+    echo Api::getWeather($matches[1]);
+}
+
+
+?>
