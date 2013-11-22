377eb163f72de34ae1f7b9a331a79f0e48a45c86
ydx <x6doooo@gmail.com>
2013-11-23 02:13:20 +0800

    调试音乐盒封装

--- linksFrontEnd/src/v303b/linksMusicBox/links-musicbox.js
+++ linksFrontEnd/src/v303b/linksMusicBox/links-musicbox.js
@@ -1,13 +1,4 @@
-//检查必要库
-(function(){
-    document.write('<link media="screen" href="http://a.links123.net/v303b/linksMusicBox/links-musicbox.css" type="text/css" rel="stylesheet" >');
-    window.jQuery || document.write('<script src="http://a.links123.net/v303b/js/jquery-1.7.1.min.js"></script>');
-    window.jQuery.fn.draggable || document.write('<script src="http://a.links123.net/v303b/js/jquery.plugins.js"></script>');
-    //window.jQuery.fn.cookies || document.write('<script src="http://a.links123.cn/v303b/js/jquery.cookies.2.2.0.min.js"></script>');
-})();
-
-
-$(function(){
+function linksMusicBoxLoad(){
 
     /* 用户配置参数，稍后开放（参数参见MusicBox.config）
     window.linksMusicBoxConfig = {};
@@ -53,14 +44,14 @@ $(function(){
     var MusicBox = {
         Init: function(){
             var self = this;
-            $('body').append(tpl);
+            jQuery('body').append(tpl);
 
             self.config = {
                 view: 'mini',   //初始视图
                 autoPlay: false     //初始是否自动播放
             };
 
-            $.extend(self.config, window.linksMusicBoxConfig);
+            jQuery.extend(self.config, window.linksMusicBoxConfig);
 
             self.music_channel_list = [
                 {
@@ -109,18 +100,18 @@ $(function(){
             self.changeMode(self.config.view, true);
             var lis = '';
             var divs = '';
-            $.each(self.music_channel_list, function(k, v){
+            jQuery.each(self.music_channel_list, function(k, v){
                 lis += '<li><a class="normal_music_channel_btn normal_music_channel_' + v.id + '" href="javascript:;" data-url="' + v.url + '" data-channel="' + v.id + '"></a></li>'
                 divs += '<div class="mini_music_channel_btn mini_music_channel_' + v.id + '" data-url="' + v.url + '" data-channel="' + v.id + '"><b></b><span>' + v.name + '</span></div>';
             });
-            $('.normal_music_channel_list').find('ul').html(lis);
-            $('.mini_channel_list').html(divs);
+            jQuery('.normal_music_channel_list').find('ul').html(lis);
+            jQuery('.mini_channel_list').html(divs);
 
-            $('#J_box_music').show();
+            jQuery('#J_box_music').show();
 
             //默认播放第一个频道
-            //if($.cookies.get('music_box_v303_close') != 1){
-                //$('.music-close-tip').show();   // 关闭提示
+            //if(jQuery.cookies.get('music_box_v303_close') != 1){
+                //jQuery('.music-close-tip').show();   // 关闭提示
             //    self.play();
             //    self.pause();
             //}else{
@@ -129,91 +120,91 @@ $(function(){
             //}
 
             //绑定事件
-            $('.normal_music_channel_list').on('click', '.normal_music_channel_btn', function(){
-                var id = $(this).attr('data-channel');
-                var url = $(this).attr('data-url');
+            jQuery('.normal_music_channel_list').on('click', '.normal_music_channel_btn', function(){
+                var id = jQuery(this).attr('data-channel');
+                var url = jQuery(this).attr('data-url');
                 self.play(id, url);
             });
-            $('.mini_channel_list').on('click', '.mini_music_channel_btn', function(){
-                var id = $(this).attr('data-channel');
-                var url = $(this).attr('data-url');
+            jQuery('.mini_channel_list').on('click', '.mini_music_channel_btn', function(){
+                var id = jQuery(this).attr('data-channel');
+                var url = jQuery(this).attr('data-url');
                 self.play(id, url);
                 return false;
             });
 
-            $('#J_box_music').draggable({handle: '.mini_music_channel_list'});
+            jQuery('#J_box_music').draggable({handle: '.mini_music_channel_list'});
 
-            $('.normal_music_box_size_btn, .mini_music_box_size_btn').click(function(){
-                var mode = $(this).attr('data-size');
+            jQuery('.normal_music_box_size_btn, .mini_music_box_size_btn').click(function(){
+                var mode = jQuery(this).attr('data-size');
                 self.changeMode(mode);
             });
 
-            $('.mini_channel_list').on('mouseover', '.mini_music_channel_btn', function(){
-                $('.mini_music_channel_btn').removeClass('hover_class');
-                $(this).addClass('hover_class');
+            jQuery('.mini_channel_list').on('mouseover', '.mini_music_channel_btn', function(){
+                jQuery('.mini_music_channel_btn').removeClass('hover_class');
+                jQuery(this).addClass('hover_class');
             }).on('mouseout', '.mini_music_channel_btn', function(){
-                $(this).removeClass('hover_class');
+                jQuery(this).removeClass('hover_class');
             })
 
-            $('.mini_music_channel_select').mouseover(function(){
-                if(!$('.mini_channel_list').is(':hidden')) return;
-                var h = $('.mini_channel_list').height();
-                var bt = $('.mini_music_box').css('top');
-                var bh = $('.mini_music_box').height();
-                var wh = $(window).height();
+            jQuery('.mini_music_channel_select').mouseover(function(){
+                if(!jQuery('.mini_channel_list').is(':hidden')) return;
+                var h = jQuery('.mini_channel_list').height();
+                var bt = jQuery('.mini_music_box').css('top');
+                var bh = jQuery('.mini_music_box').height();
+                var wh = jQuery(window).height();
                 bt = bt == 'auto' ? wh - bh : parseInt(bt);
                 if(wh - bt - bh <= h){
-                    $('.mini_channel_list').css('margin-top', - (h + bh - 8) + 'px');
+                    jQuery('.mini_channel_list').css('margin-top', - (h + bh - 8) + 'px');
                 }else{
-                    $('.mini_channel_list').css('margin-top', '0');
+                    jQuery('.mini_channel_list').css('margin-top', '0');
                 }
-                $('.mini_channel_list').show();
+                jQuery('.mini_channel_list').show();
             }).mouseout(function(){
-                $('.mini_music_channel_btn').removeClass('hover_class');
-                $('.mini_channel_list').hide();
+                jQuery('.mini_music_channel_btn').removeClass('hover_class');
+                jQuery('.mini_channel_list').hide();
             });
 
-            $('.normal_music_box_close_btn, .mini_music_box_close_btn, .music-close-tip').click(function(){
+            jQuery('.normal_music_box_close_btn, .mini_music_box_close_btn, .music-close-tip').click(function(){
                 self.close();
             });
 
-            $('.normal_music_channel_list_toggle').click(function(){
+            jQuery('.normal_music_channel_list_toggle').click(function(){
                 self.changeNormalPosition();
             });
 
-            $(document).on('keydown', function(e){
+            jQuery(document).on('keydown', function(e){
                 if (e.keyCode == 27) {
-                    var mode1 = $('.active-size').hasClass('size_fullscreen');
-                    var mode2 = $('.active-size').hasClass('size_normal');
+                    var mode1 = jQuery('.active-size').hasClass('size_fullscreen');
+                    var mode2 = jQuery('.active-size').hasClass('size_normal');
                     if(mode1){
                         self.changeMode('normal');
-                    }else if(mode2 && parseInt($('#J_box_music').css('left')) == 0){
+                    }else if(mode2 && parseInt(jQuery('#J_box_music').css('left')) == 0){
                         self.changeNormalPosition();
                     }
                 }
             });
 
-            $('.normal_music_box_play_btn').click(function(){
-                if($(this).hasClass('normal_music_box_play_btn_play')){
+            jQuery('.normal_music_box_play_btn').click(function(){
+                if(jQuery(this).hasClass('normal_music_box_play_btn_play')){
                     self.play();
                 }else{
                     self.pause();
                 }
             });
-            $('.mini_music_box_play_btn').click(function(){
-                if($(this).hasClass('mini_music_box_play_btn_play')){
+            jQuery('.mini_music_box_play_btn').click(function(){
+                if(jQuery(this).hasClass('mini_music_box_play_btn_play')){
                     self.play();
                 }else{
                     self.pause();
                 }
             });
 
-            $('.normal_music_iframe_box_pause_status a').click(function(){
+            jQuery('.normal_music_iframe_box_pause_status a').click(function(){
                 self.play();
             });
 
-            $('.mini_current_channel').click(function(){
-                if(!$('#K_303_music_iframe').attr('src')){
+            jQuery('.mini_current_channel').click(function(){
+                if(!jQuery('#K_303_music_iframe').attr('src')){
                     self.play();
                 }
             });
@@ -224,23 +215,23 @@ $(function(){
 
         },
         close: function(){
-            $('#J_box_music').hide();
-            $('#K_303_music_iframe').attr('src', '');
-            //$.cookies.set('music_box_v303_close', '1', { expiresAt: (new Date).add_day(365) });
+            jQuery('#J_box_music').hide();
+            jQuery('#K_303_music_iframe').attr('src', '');
+            //jQuery.cookies.set('music_box_v303_close', '1', { expiresAt: (new Date).add_day(365) });
 
         },
         pause: function(){
-            $('.normal_music_box_play_btn').removeClass('normal_music_box_play_btn_pause').addClass('normal_music_box_play_btn_play');
-            $('.mini_music_box_play_btn').removeClass('mini_music_box_play_btn_pause').addClass('mini_music_box_play_btn_play');
+            jQuery('.normal_music_box_play_btn').removeClass('normal_music_box_play_btn_pause').addClass('normal_music_box_play_btn_play');
+            jQuery('.mini_music_box_play_btn').removeClass('mini_music_box_play_btn_pause').addClass('mini_music_box_play_btn_play');
             
-            $('#K_303_music_iframe').attr('src', '').hide();
-            $('.normal_music_iframe_box').addClass('normal_music_iframe_box_pause');
+            jQuery('#K_303_music_iframe').attr('src', '').hide();
+            jQuery('.normal_music_iframe_box').addClass('normal_music_iframe_box_pause');
 
         },
         play: function(id, url){
             var self = this;
             if(arguments.length == 0){
-                var cur = $('.normal_music_channel_list').find('.active');
+                var cur = jQuery('.normal_music_channel_list').find('.active');
                 if(cur.size()){
                     id = cur.find('a').attr('data-channel');
                     url = cur.find('a').attr('data-url');
@@ -250,36 +241,36 @@ $(function(){
                     url = defaultChannel.url;
                 }
             }
-            $('.normal_music_iframe_box').removeClass('normal_music_iframe_box_pause');
-            $('.normal_music_box_play_btn').removeClass('normal_music_box_play_btn_play').addClass('normal_music_box_play_btn_pause');
-            $('.mini_music_box_play_btn').removeClass('mini_music_box_play_btn_play').addClass('mini_music_box_play_btn_pause');
+            jQuery('.normal_music_iframe_box').removeClass('normal_music_iframe_box_pause');
+            jQuery('.normal_music_box_play_btn').removeClass('normal_music_box_play_btn_play').addClass('normal_music_box_play_btn_pause');
+            jQuery('.mini_music_box_play_btn').removeClass('mini_music_box_play_btn_play').addClass('mini_music_box_play_btn_pause');
 
-            $('.normal_music_channel_list').find('li').removeClass('active');
-            $('.normal_music_channel_' + id).closest('li').addClass('active');
-            $('#K_303_music_iframe').attr('src', url).show();
-            $('.mini_current_channel').find('.mini_music_channel_btn').appendTo('.mini_channel_list');
-            $('.mini_music_channel_' + id).appendTo('.mini_current_channel');
+            jQuery('.normal_music_channel_list').find('li').removeClass('active');
+            jQuery('.normal_music_channel_' + id).closest('li').addClass('active');
+            jQuery('#K_303_music_iframe').attr('src', url).show();
+            jQuery('.mini_current_channel').find('.mini_music_channel_btn').appendTo('.mini_channel_list');
+            jQuery('.mini_music_channel_' + id).appendTo('.mini_current_channel');
         },
         changeMode: function(mode, isFirst, iconClick){
             var self = this;
-            if(!isFirst && parseInt($('#J_box_music').css('left')) != 0){
+            if(!isFirst && parseInt(jQuery('#J_box_music').css('left')) != 0){
                 self.changeNormalPosition();
             }
 
-            var t = $(window).scrollTop();
-            var h = $(window).height() / 2;
+            var t = jQuery(window).scrollTop();
+            var h = jQuery(window).height() / 2;
 
-            $('#J_box_music').attr('class', '').addClass(mode +'_music_box');
+            jQuery('#J_box_music').attr('class', '').addClass(mode +'_music_box');
 
             if(mode == 'normal'){
-                if($(window).height() < 560){
-                    $('.normal_music_box').css({
+                if(jQuery(window).height() < 560){
+                    jQuery('.normal_music_box').css({
                         'bottom': 'auto',
                         'top' : '0',
                         'position' : 'absolute'
                     });
                 }else{
-                    $('.normal_music_box').css({
+                    jQuery('.normal_music_box').css({
                         'bottom': 0, //-t, // 这里fixed了，bottom为负值，啥意思？
                         'top' : 'auto',
                         'position' : 'fixed'
@@ -287,57 +278,99 @@ $(function(){
                 }
             }
             if(mode == 'mini'){
-                $('.mini_music_box').css({
+                jQuery('.mini_music_box').css({
                     'bottom' : 0,// '80px',//-t + h,
                     'top' : 'auto'
                 });
             }
             if(mode == 'fullscreen'){
-                $('.fullscreen_music_box').css({
+                jQuery('.fullscreen_music_box').css({
                     bottom:0,
                     top:0,
                     left:0,
                     right:0
                 });
-                var ww = $('.normal_music_iframe_box').width();
-                var hh = $('.normal_music_iframe_box').height();
-                $('#K_303_music_iframe').width(ww - 40).height(hh - 40);
+                var ww = jQuery('.normal_music_iframe_box').width();
+                var hh = jQuery('.normal_music_iframe_box').height();
+                jQuery('#K_303_music_iframe').width(ww - 40).height(hh - 40);
             }else{
-                $('#K_303_music_iframe').width(749).height(508);
+                jQuery('#K_303_music_iframe').width(749).height(508);
             }
 
-            $('.normal_music_box_size_btn, .mini_music_box_size_btn').removeClass('active-size');
-            $('.size_' + mode).addClass('active-size');
+            jQuery('.normal_music_box_size_btn, .mini_music_box_size_btn').removeClass('active-size');
+            jQuery('.size_' + mode).addClass('active-size');
         },
         changeNormalPosition: function(){
             var self = this;
-            var mode = $('.active-size').hasClass('size_fullscreen');
+            var mode = jQuery('.active-size').hasClass('size_fullscreen');
             if(mode){
                 self.changeMode('normal');
             }
 
-            var st = $('#J_box_music').css('left');
+            var st = jQuery('#J_box_music').css('left');
             var tit, pos;
 
             st = parseInt(st);
             if(st != 0){
                 tit = '收起';
                 pos = '0';
-                $('.normal_music_channel_list_toggle').find('span').removeAttr('class').addClass('list-close');
-                if(!$('#K_303_music_iframe').attr('src')){
+                jQuery('.normal_music_channel_list_toggle').find('span').removeAttr('class').addClass('list-close');
+                if(!jQuery('#K_303_music_iframe').attr('src')){
                     self.play();
                 }
             }else{
                 tit = '展开';
                 pos = '-789px';
-                $('.normal_music_channel_list_toggle').find('span').removeAttr('class').addClass('list-open');
+                jQuery('.normal_music_channel_list_toggle').find('span').removeAttr('class').addClass('list-open');
             }
-            $('#J_box_music').animate({'left': pos});
-            $('.normal_music_box_toggle_btn').html(tit);
+            jQuery('#J_box_music').animate({'left': pos});
+            jQuery('.normal_music_box_toggle_btn').html(tit);
         }
     };
 
     MusicBox.Init();
     
+}
 
-});
+//检查必要库
+(function(){
+
+    function loadScript(url , callback){   
+        var script = document.createElement("script");   
+        script.type="text/javascript";   
+        if(script.readyState){   
+            script.onreadystatechange = function(){   
+                if(script.readyState=="loaded"||script.readyState=="complete"){   
+                    script.onreadystatechange=null;   
+                    callback();
+                }   
+            }   
+        }else{   
+            script.onload = function(){
+                callback();   
+            }   
+        }   
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);   
+    }      
+    document.write('<link media="screen" href="http://a.links123.net/v303b/linksMusicBox/links-musicbox.css" type="text/css" rel="stylesheet" >');
+    if(!window.jQuery){
+        loadScript('http://a.links123.net/v303b/js/jquery-1.7.1.min.js', function(){
+            if(!window.jQuery.fn.draggable){
+                loadScript('http://a.links123.net/v303b/js/jquery.plugins.js', function(){
+                    linksMusicBoxLoad();
+                });
+            }else{;
+                linksMusicBoxLoad();
+            }
+        });
+    }else{
+        if(!window.jQuery.fn.draggable){
+            loadScript('http://a.links123.net/v303b/js/jquery.plugins.js', function(){
+                linksMusicBoxLoad();
+            });
+        }else{
+            linksMusicBoxLoad();
+        }
+    }
+})();
