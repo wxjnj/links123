820888bef08e28c6c072675a336f6b97e09a0fd1
spring <spring@links123.cn>
2013-12-02 23:15:51 +0800

    Modify

--- links123/src/Weixin/test.php
+++ links123/src/Weixin/test.php
@@ -11,8 +11,15 @@ include "Api.class.php";
 $matches=array();
 if(preg_match("/天气@(\W+)/","天气@上海",$matches)){
    // print_r($matches);
-    echo Api::getWeather($matches[1]);
-}
+    $url="http://php.weather.sina.com.cn/xml.php?city=".mb_convert_encoding($matches[1], 'GB2312', 'UTF-8')."&password=DJOYnieT8234jlsK&day=0";
+    $content=file_get_contents($url);
+    echo $content;
+    exit;
 
+}
+if(preg_match("/翻译@(\W+)/","翻译@上海",$matches)){
+    print_r($matches);
+    echo Api::translate($matches[1]);
+}
 
 ?>
