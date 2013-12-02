8bd3c012f907d75445bcb6d8c5bdb84c0997b245
spring <spring@links123.cn>
2013-12-02 21:50:45 +0800

    Modify translate function

--- links123/src/Weixin/Api.class.php
+++ links123/src/Weixin/Api.class.php
@@ -48,7 +48,7 @@ class  Api {
     }
 
     public  static function translate($input){
-        $url="http://fanyi.youdao.com/openapi.do?keyfrom=links123cn&key=1695588868&type=data&doctype=json&version=1.1&q=".urlencode("测试");
+        $url="http://fanyi.youdao.com/openapi.do?keyfrom=links123cn&key=1695588868&type=data&doctype=json&version=1.1&q=".urlencode($input);
         $content=file_get_contents($url);
         $returnstr="";
         if($content){
