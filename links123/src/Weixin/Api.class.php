c95b900c4a2a691251e86a0b9cce6c5500c7cc23
spring <spring@links123.cn>
2013-12-02 21:48:24 +0800

    Mofify translate function

--- links123/src/Weixin/Api.class.php
+++ links123/src/Weixin/Api.class.php
@@ -11,8 +11,10 @@ class  Api {
         $url="http://php.weather.sina.com.cn/xml.php?city=".urlencode("$city")."&password=DJOYnieT8234jlsK&day=0";
          $content=file_get_contents($url);
          $xml= simplexml_load_string($content);
-         $returnstr=$city."天气预报\n".$xml->Weather["status1"]."\t".$xml->Weather["direction1"].$xml->Weather["power1"]
-                    ."级";
+        $returnstr=$city."天气预报\n".$xml->Weather->status1."\t".$xml->Weather->direction1.$xml->Weather->power1
+            ."级\n温度：".$xml->Weather->temperature2."~".$xml->Weather->temperature1."度，"
+            ."\n紫外线：".$xml->Weather->zwx_l."\n洗车指数：".$xml->Weather->xcz_s."\n穿衣指数：".$xml->Weather->chy_shuoming;
+
         return  $returnstr;
     }
 
@@ -46,9 +48,16 @@ class  Api {
     }
 
     public  static function translate($input){
-        $url="http://fanyi.youdao.com/openapi.do?keyfrom=links123cn&key=1695588868&type=data&doctype=json&version=1.1&q=$input";
+        $url="http://fanyi.youdao.com/openapi.do?keyfrom=links123cn&key=1695588868&type=data&doctype=json&version=1.1&q=".urlencode("测试");
         $content=file_get_contents($url);
-        $jsonobj=json_decode($content);
-        return $jsonobj->translation;
+        $returnstr="";
+        if($content){
+            $jsonobj=json_decode($content);
+
+            foreach($jsonobj->translation as $value){
+                $returnstr.=$value.";";
+            }
+        }
+        return rtrim($returnstr,';');
     }
 }
