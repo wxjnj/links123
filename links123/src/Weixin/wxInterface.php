c70e5b533d0939d68639868c0d89f20d0d456d99
spring <spring@links123.cn>
2013-12-13 21:38:19 +0800

    Add gn news

--- links123/src/Weixin/wxInterface.php
+++ links123/src/Weixin/wxInterface.php
@@ -24,7 +24,10 @@ switch($type) {
         if($userinput=='帮助'||$userinput=='?'||$userinput=='？'){
             $wxObj->text("你好，这是另客网的微信订阅号，输入'？','?'或者'帮助'来查看常用命令，微信订阅提供天气查询（如：输入'天气@上海'，查询上海的天气），".
                     "简单的英汉翻译（如：输入'翻译@美国'，得到美国的英文），还有简单的对话功能。")->reply();
-        }else if($userinput=='视频'){
+        }else if($userinput=='国内新闻'){
+            $wxObj->text(Api::getGNNews(5))->reply();
+        }
+        else if($userinput=='视频'){
             $wxObj->text("测试视频：http://wap.qtw365.com/demo.html")->reply();
         }else
         if(preg_match("/翻译\@(.*)/",$userinput,$matches)){
