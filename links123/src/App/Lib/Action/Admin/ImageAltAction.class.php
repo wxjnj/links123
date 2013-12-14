579ed3c68c2070d9064362027dda7ed07ca701ec
spring <spring@links123.cn>
2013-12-14 16:43:41 +0800

    Add ImageAlt

---
+++ links123/src/App/Lib/Action/Admin/ImageAltAction.class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 13-12-14
+ * Time: 下午4:09
+ */
+
+class ImageAltAction  extends  CommentAction{
+        public function index(){
+            $arr=$this->getImages($_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR."Public");
+            print_r($arr);
+            $this->show("Test");
+        }
+        private function getImages($dir){
+            $image_type=array("jpg",'jpeg','gif','png');
+           if(! $handle=@opendir($dir)){
+               return false;
+           }
+            $arr=array();
+           while(false!==($file=readdir($handle))){
+               if($file!=='.'||$file!=='..'){
+                   $file=$dir.DIRECTORY_SEPARATOR.$file;
+                   if(is_dir($file)){
+                       $temparr=$this->getImages($file);
+                       $arr=array_merge($arr,$temparr);
+                   }
+                   else{
+                       $file_ext=  end(explode('.', $file));
+                       if(in_array(strtolower($file_ext),$image_type)){
+                           $arr[]=$file;
+                       }
+                   }
+               }
+               return $arr;
+
+           }
+
+        }
+} 
